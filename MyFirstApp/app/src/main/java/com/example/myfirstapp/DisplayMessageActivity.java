691e5778a536dc818a2992095b59f639fa7ac6f6
nazanin-tabatabaei <nazanin.tbt@gatech.edu>
2021-02-08 00:31:41 -0500

    database multi entry

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
@@ -13,6 +14,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class DisplayMessageActivity extends AppCompatActivity {
     private Button mSendData;
+    private EditText mKeyValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +28,20 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
-
+        mKeyValue=(EditText) findViewById(R.id.keyValue) ;
 
 
         mSendData=(Button) findViewById(R.id.sendData);
         mSendData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String key=mKeyValue.getText().toString();
                 // Write a message to the database
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("message");
+                DatabaseReference myRef = database.getReference("Messages").child(key);
+
+                //myRef.child(key).setValue(message);
+                //myRef.setValue(message);
                 myRef.setValue(message);
             }
         });
