900dee2a925f4b70d518733e001b843f15658e0a
nazanin-tabatabaei <nazanin.tbt@gatech.edu>
2021-02-07 23:08:57 -0500

    send message to firebase database

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class DisplayMessageActivity extends AppCompatActivity {
+    private Button mSendData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,19 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
+
+
+
+        mSendData=(Button) findViewById(R.id.sendData);
+        mSendData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Write a message to the database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+                myRef.setValue(message);
+            }
+        });
     }
+
 }
